bb1211e9c6b4b545377849216074cd3a5a523d17
SergeyKarabanov <karabanovsg@yandex.ru>
2021-11-01 12:06:24 +0400

    edit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SergeyKarabanov/java-project-lvl1/workflows/hexlet-check/badge.svg)](https://github.com/SergeyKarabanov/java-project-lvl1/actions)
+<a href="https://codeclimate.com/github/SergeyKarabanov/java-project-lvl1/maintainability"><img src="https://api.codeclimate.com/v1/badges/1082b21bde9247681504/maintainability" /></a>
---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,18 @@
+package hexlet.code;
+
+import java.util.Scanner;
+
+public class Cli {
+
+    public static void greeting(String args) {
+        System.out.print("May I have your name? ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        System.out.println(name);
+        System.out.println("Hello," + name + "!");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
