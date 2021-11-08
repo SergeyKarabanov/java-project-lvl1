a12a73377536b29774e316c0176f7146ee27bfbb
SergeyKarabanov <karabanovsg@yandex.ru>
2021-11-08 23:32:15 +0400

    edit

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -6,12 +6,14 @@ public class App {
     static final int GAME_NUMBER1 = 1;
     static final int GAME_NUMBER2 = 2;
     static final int GAME_NUMBER3 = 3;
+    static final int GAME_NUMBER4 = 4;
 
     public static void main(String[] args) {
         System.out.println("Please enter the game number and press Enter.");
         System.out.println("1 - Greet");
         System.out.println("2 - Even");
         System.out.println("3 - Calc");
+        System.out.println("4 - GCD");
         System.out.println("0 - Exit");
         Scanner scan = new Scanner(System.in);
         int number = scan.nextInt();
@@ -24,6 +26,9 @@ public class App {
         }
         if (number == GAME_NUMBER3) {
             Engine.calc();
+        }
+        if (number == GAME_NUMBER4) {
+            Engine.gCD();
         }
     }
 }
--- src/main/java/hexlet/code/Engine.java
+++ src/main/java/hexlet/code/Engine.java
@@ -1,6 +1,7 @@
 
 package hexlet.code;
 
+import hexlet.code.games.GameFour;
 import hexlet.code.games.GameOne;
 import hexlet.code.games.GameThree;
 import hexlet.code.games.GameTwo;
@@ -36,6 +37,23 @@ public class Engine {
             int result2 = GameThree.questionForCalc();
             if (result2 == 1) {
                 int result3 = GameThree.questionForCalc();
+                if (result3 == 1) {
+                    System.out.println("Congratulations," + name);
+                }
+            }
+        }
+        if (result == 0) {
+            System.out.println("Let's try again," + name);
+        }
+    }
+
+    public static void gCD() {
+        String name = GameOne.nameigrok();
+        int result = GameFour.questionsForGCD();
+        if (result == 1) {
+            int result2 = GameFour.questionsForGCD();
+            if (result2 == 1) {
+                int result3 = GameFour.questionsForGCD();
                 if (result3 == 1) {
                     System.out.println("Congratulations," + name);
                 }
